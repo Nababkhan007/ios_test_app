d18ca4e05b14495d3bda24690655c03cb6ad04e5
Nababkhan007 <markohenry46@gmail.com>
2021-12-25 15:30:01 +0600

    Initial commit

---
+++ test_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test_app
+//
+//  Created by Milton on 12/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test_app/test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_appApp.swift
+//  test_app
+//
+//  Created by Milton on 12/25/21.
+//
+
+import SwiftUI
+
+@main
+struct test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
